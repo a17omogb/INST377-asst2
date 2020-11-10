a8e24174fe9b7faaa6087148159ac4ade7782670
chand9812 <chand98@terpmail.umd.edu>
2020-11-09 22:01:35 -0500

    getting data and experimenting

--- public/script.js
+++ public/script.js
@@ -1,1 +1,23 @@
-dsdfa sdafsdf 
+const source_url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
+const data = [];
+
+fetch(source_url).then(blob => blob.json()).then(info => data.push(...info));
+console.log(data);
+
+function matchFinding(word, data_param){
+    return data_param.filter(item => {
+        const reword = new RegExp(word, 'gi');
+        return item.city.match(reword) || item.category.match(reword)
+    });
+}
+
+function displayMatches(){
+    const matcharray = matchFinding(this.value, data);
+    console.log(matcharray);
+}
+
+const searchInput = document.querySelector('.search');
+
+
+document.body.addEventListener('keyup', displayMatches);
